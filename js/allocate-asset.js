74f1dd1bf005a6ef988e1cb7ca76d232fc49b57d
itff-tech <it@finalfunnel.com>
2026-03-06 19:29:14 +0530

    Update allocate-asset.js

--- js/allocate-asset.js
+++ js/allocate-asset.js
@@ -1,19 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js"; 
-import { getFirestore, getDocs, collection, doc, updateDoc } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
-
-// Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyAspahfUUGnBzh0mh6U53evGQzWQP956xQ",
-  authDomain: "ffassetmanager.firebaseapp.com",
-  projectId: "ffassetmanager",
-  storageBucket: "ffassetmanager.appspot.com",
-  messagingSenderId: "803858971008",
-  appId: "1:803858971008:web:72d69ddce6cbc85010a965"
-};
-
-// Init Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import { getDocs, collection, doc, updateDoc } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+import { db } from "../firebase-client.js";
 const assetsCollection = collection(db, "assets");
 
 document.addEventListener('DOMContentLoaded', async function () {
