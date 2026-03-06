43a698f005e4c310360f99eacf288591a73d2914
itff-tech <it@finalfunnel.com>
2026-03-06 19:02:53 +0530

    firebase-client.js

---
+++ firebase-client.js
@@ -0,0 +1,18 @@
+import { initializeApp, getApp, getApps } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAspahfUUGnBzh0mh6U53evGQzWQP956xQ",
+  authDomain: "ffassetmanager.firebaseapp.com",
+  projectId: "ffassetmanager",
+  storageBucket: "ffassetmanager.appspot.com",
+  messagingSenderId: "803858971008",
+  appId: "1:803858971008:web:72d69ddce6cbc85010a965"
+};
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
