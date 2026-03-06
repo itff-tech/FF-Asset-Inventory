2abd852f589ad1d953e0d2a2c0d7980c45680a53
itff-tech <it@finalfunnel.com>
2026-03-06 20:14:16 +0530

    Update firebaseConfig.js

--- firebaseConfig_old.js
+++ firebaseConfig_old.js
@@ -1,18 +1,5 @@
 // firebaseConfig.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAspahfUUGnBzh0mh6U53evGQzWQP956xQ",
-  authDomain: "ffassetmanager.firebaseapp.com",
-  databaseURL: "https://ffassetmanager-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "ffassetmanager",
-  storageBucket: "ffassetmanager.firebasestorage.app",
-  messagingSenderId: "803858971008",
-  appId: "1:803858971008:web:72d69ddce6cbc85010a965"
-};
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const assetsCollection = collection(db, 'assets');
-
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+import { db } from "./firebase-client.js";
+const assetsCollection = collection(db, "assets");
 export { db, assetsCollection, getDocs };
