dcd57b9a28988423abc3ff0a358f4ac6240617f9
itff-tech <it@finalfunnel.com>
2026-03-06 19:05:12 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,24 +1,9 @@
-// auth.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import {
-  getAuth,
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAspahfUUGnBzh0mh6U53evGQzWQP956xQ",
-  authDomain: "ffassetmanager.firebaseapp.com",
-  projectId: "ffassetmanager",
-  storageBucket: "ffassetmanager.appspot.com",
-  messagingSenderId: "803858971008",
-  appId: "1:803858971008:web:72d69ddce6cbc85010a965"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
+import { auth } from "./firebase-client.js";
 window.login = function () {
   const usernameInput = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value;
