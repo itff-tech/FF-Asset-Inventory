1a13b644ceedace8c36fd1dc611377323f0cf690
itff-tech <it@finalfunnel.com>
2026-03-10 22:28:04 +0530

    Update add-asset-page.js

--- js/add-asset-page.js
+++ js/add-asset-page.js
@@ -145,7 +145,7 @@ function showToast(message, type = "info", duration = 3000) {
     };
 
     await addDoc(assetsCollection, assetData);
-    alert(`✅ Asset ${assetId} added successfully!`);
+    showToast("✅ Asset ${assetId} Added successfully!", "success");
     document.getElementById("assetForm").reset();
     await loadAssetTypes();
   } catch (error) {
