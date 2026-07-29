0924488c689e99585681fc7881aae1830ee0500a
IT-FinalFunnel <it@finalfunnel.com>
2026-07-29 19:28:13 +0530

    Delete firebaseConfig_old.js

--- firebaseConfig_old.js
+++
@@ -1,5 +0,0 @@
-// firebaseConfig.js
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
-import { db } from "./firebase-client.js";
-const assetsCollection = collection(db, "assets");
-export { db, assetsCollection, getDocs };
